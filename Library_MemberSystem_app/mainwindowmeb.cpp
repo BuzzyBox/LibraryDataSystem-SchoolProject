10d65270933b45354e49b5d8c4d75493413c0bf0
BuzzyBox <270121466@yoobeestudent.ac.nz>
2021-11-17 00:08:24 +1300

    Member Application Update 3
    This is the last update before I completely changed the concept and layout for the library application menu. Since there is only a few days left before we have to get this project done, I had to made some big changes for this application to make things easier for me and Tam.

--- Library_MemberSystem_app/mainwindowmeb.cpp
+++ Library_MemberSystem_app/mainwindowmeb.cpp
@@ -6,9 +6,7 @@
 #include <QMessageBox>
 #include <QFileDialog>
 #include <QStandardPaths>
-#include <QResource>
 #include <QDir>
-#include <QFileDialog>
 
 MainWindowMeb::MainWindowMeb(QWidget *parent)
     : QMainWindow(parent)
@@ -24,12 +22,6 @@ MainWindowMeb::MainWindowMeb(QWidget *parent)
 
     getBookData();
     ui->ItemBook->setCurrentRow(0);
-
-
-
-
-
-
 
 }
 
@@ -52,23 +44,6 @@ void MainWindowMeb::whenBookClicked()
 {
     int index = ui->ItemBook->currentRow();
 
-//    QString fileName;
-//    fileName = QFileDialog::getOpenFileName(this,"Open Image","./","Image File(*.png *.jpg");
-
-//    if(fileName != "")
-//    {
-//        int lastSlash = fileName.lastIndexOf("/");
-//        QString shortName = fileName.right(fileName.size()-lastSlash-1);
-
-//        QFile::copy(fileName,"./images/"+shortName);
-//        QPixmap pixmap("./images/"+shortName);
-//        ui->labBookCover->setPixmap(pixmap);
-//        ui->labBookCover->setScaledContents(true);
-
-//        imageFilePath = "./images/"+shortName;
-
-//    }
-
 
 
     if(index >= 0)
@@ -83,53 +58,51 @@ void MainWindowMeb::whenBookClicked()
         ui->lbIDNum->setText(newBook->getbookID());
         ui->lbGenreTxt->setText(newBook->getGenre());
         ui->lbAvailability->setText(newBook->getAvail());
-        //ui->lbDateBack->setText(newBook->getDueBack());
 
+
+//        QDir imageDir("C:/Documents", "C:/Images");
+
+//        QString findImage = imageDir.relativeFilePath("./images");
+
+//        QPixmap pixMap(findImage);
+
+
+
+//        int lastSlash=fileName.lastIndexOf("/");
+//        QString shortName=fileName.right(fileName.size()-lastSlash-1);
+//        QFile::copy(fileName,"./images/"+shortName);
+//        QPixmap pixMap("./images/"+shortName);
+//        imageFilePath="./images/"+shortName;
         QPixmap pixmap(newBook->getImageFilePath());
-
+//        pixMap.loadFromData("jpeg", "png");
+//        ui->labBookCover->setPixmap(pixMap);
         ui->labBookCover->setPixmap(pixmap);
 
         ui->labBookCover->setScaledContents(true);
 
     }
 
-
 }
 
 void MainWindowMeb::getBookData()
 {
 
-    //C:\Users\Buzzy\Documents\QtCode\build-Library_DataSystem_1-Desktop_Qt_6_2_0_MinGW_64_bit-Debug
-
 
 
     QDir Bookdir("C:/Documents");
-
     QString findbooks;
-
     findbooks = Bookdir.relativeFilePath("/build-Library_DataSystem_1-Desktop_Qt_6_2_0_MinGW_64_bit-Debug/book.txt");
 
     QFile loadBook(findbooks);
 
 
 
+    if(!loadBook.open(QIODevice::ReadOnly|QIODevice::Text))
+    {
 
+       QMessageBox::warning(this,"BookNook Library", "Cannot find books");
 
-
-
-
-//      QString loadBook;("./book.txt");
-//      QFile loadB(loadBook);
-
-
-
-
-              if(!loadBook.open(QIODevice::ReadOnly|QIODevice::Text))
-              {
-
-                  QMessageBox::warning(this,"BookNook Library", "Cannot find books");
-
-              }
+    }
 
              loadBook.open(QIODevice::ReadOnly|QIODevice::Text);
              QTextStream in(&loadBook);
@@ -149,106 +122,18 @@ void MainWindowMeb::getBookData()
 
                   ui->ItemBook->addItem(info.at(0)+ "               "+info.at(1) );
                   Book* temp = new Book(info.at(0),info.at(1),info.at(2),info.at(3),info.at(4),info.at(5).toInt(),info.at(6),info.at(7),info.at(8), info.at(9));
+//                  ui->labBookCover->setPixmap(info.at(9));
+//                  ui->labBookCover->setScaledContents(true);
                   booklist.push_back(temp);
 
-              }
-
-
+     }
 
               in.flush();
               loadBook.close();
 
 
-//    QFile bookFile(":/resources/res/book.txt");
 
 
-//        if(!bookFile.open(QIODevice::ReadOnly|QIODevice::Text))
-//        {
-
-//            QMessageBox::warning(this,"BookNook Library", "Cannot find books");
-
-//        }
-
-//        bookFile.open(QIODevice::ReadOnly|QIODevice::Text);
-//        QTextStream in(&bookFile);
-
-
-//        ui->ItemBook->clear();
-//        for(int i = 0; i < booklist.size(); i++)
-//        {
-//            delete booklist.at(i);
-//        }
-//        booklist.clear();
-
-//        while(!in.atEnd())
-//        {
-//            QString line = in.readLine();
-//            QStringList info = line.split(",");
-
-//            ui->ItemBook->addItem(info.at(0)+ "               "+info.at(1) );
-//            Book* temp = new Book(info.at(0),info.at(1),info.at(2),info.at(3),info.at(4),info.at(5).toInt(),info.at(6),info.at(7),info.at(8), info.at(9));
-//            booklist.push_back(temp);
-
-//        }
-
-
-
-//        in.flush();
-//        bookFile.close();
-
-
-
-
-
-
-
-//D:\Users\270121466\Documents\build-Library_DataSystem_1-Desktop_Qt_5_12_11_MinGW_64_bit-Debug
-
-//    QString fileBook("book.txt");
-
-
-//    if(!bookFile.open(QIODevice::ReadOnly | QIODevice:: Text))
-//    {
-//        QMessageBox::warning(this,"BookNook Library", "Cannot find books");
-//    }
-
-//    if(QFileInfo::exists(fileBook))
-//    {
-
-//        QFile bookFile(fileBook);
-//        bookFile.open(QIODevice::ReadOnly|QIODevice::Text);
-//        QTextStream in(&bookFile);
-
-
-//        ui->ItemBook->clear();
-//        for(int i = 0; i < booklist.size(); i++)
-//        {
-//            delete booklist.at(i);
-//        }
-//        booklist.clear();
-
-//        while(!in.atEnd())
-//        {
-//            QString line = in.readLine();
-//            QStringList info = line.split(",");
-
-//            ui->ItemBook->addItem(info.at(0)+ "               "+info.at(1) );
-//            Book* temp = new Book(info.at(0),info.at(1),info.at(2),info.at(3),info.at(4),info.at(5).toInt(),info.at(6),info.at(7),info.at(8), info.at(9));
-//            booklist.push_back(temp);
-
-//        }
-
-
-
-//        in.flush();
-//        bookFile.close();
-
-
-//    }
-//    else
-//    {
-//         QMessageBox::warning(this,"BookNook Library", "Cannot find books");
-//    }
 
 
 }
